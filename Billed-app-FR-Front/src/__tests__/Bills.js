10dc8f5b45b80f6182f2c2b0b80688a70714414e
Habib <elmouini.habib@gmail.com>
2022-12-04 22:31:01 +0100

    Test unitaire et d'intégration + error 404/500 for Bills

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -1,17 +1,17 @@
 /**
  * @jest-environment jsdom
  */
-
-import { screen, waitFor } from "@testing-library/dom";
+import "@testing-library/jest-dom";
+import { fireEvent, screen, waitFor } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
 import { ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
-
+import Bills from "../containers/Bills.js";
 import router from "../app/Router.js";
-
 import mockStore from "../__mocks__/store";
-jest.mock("../app/Store", () => mockStore);
+
+//jest.mock("../app/Store", () => mockStore);
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -47,5 +47,112 @@ describe("Given I am connected as an employee", () => {
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
     });
+  });
+});
+
+//Given - when - then
+describe("Given I am connected as an employee", () => {
+  describe("When I am on Bills Page", () => {
+    test("Then if I click on the eye icon a modal should appear", async () => {
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+      await waitFor(() => screen.getAllByTestId("icon-eye"));
+      const iconEye = screen.getAllByTestId("icon-eye");
+      // Fonctions simulées https://jestjs.io/fr/docs/mock-function-api#:~:text=Les%20fonctions%20simul%C3%A9es%20sont%20%C3%A9galement,fn()%20.
+      $.fn.modal = jest.fn();
+      // fireEvent declenchement d'événements du DOM https://testing-library.com/docs/dom-testing-library/api-events/
+      fireEvent.click(iconEye[0]);
+      // getBy =renvoie le nœud correspondant pour une requête et renvoie une erreur descriptive si aucun élément ne correspond ou si plusieurs correspondances sont trouvées
+      //https://testing-library.com/docs/queries/about/
+      //getAllBy= renvoie un tableau de tous les nœuds correspondants pour une requête et génère une erreur si aucun élément ne correspond
+      await waitFor(() => screen.getByText("Justificatif"));
+      const uploadedDoc = screen.getAllByText("Justificatif");
+      expect(uploadedDoc).toBeTruthy();
+    });
+  });
+});
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on Bills Page", () => {
+    test("Then if I click on the button 'Nouvelle note de frais a bill form will appear", async () => {
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+      await waitFor(() => screen.getByTestId("btn-new-bill"));
+      const billButton = screen.getByTestId("btn-new-bill");
+      fireEvent.click(billButton);
+      await waitFor(() => screen.getByTestId("form-new-bill"));
+      const billForm = screen.getByTestId("form-new-bill");
+      expect(billForm).toBeTruthy();
+    });
+  });
+});
+
+//////////////////////////////////
+describe("Given I am connected as an employee", () => {
+  describe("When I am on Bills Page and the page is loaded", () => {
+    test("Then the function named getBills has to be launched", async () => {
+      const pageContent = BillsUI({ data: bills });
+      document.body.innerHTML = pageContent;
+
+      const mockObject = new Bills({
+        document,
+        onNavigate,
+        store: mockStore,
+        localStorage: localStorageMock,
+      });
+
+      jest.spyOn(mockObject, "getBills");
+      const result = await mockObject.getBills();
+      expect(result[0]["name"]).toBe(bills[0]["name"]);
+
+      const pageTitle = await screen.getByText("Mes notes de frais");
+      const newBillButton = await screen.getByTestId("btn-new-bill");
+
+      expect(pageTitle).toBeTruthy();
+      expect(newBillButton).toBeTruthy();
+      expect(screen.getAllByTestId("icon-eye")).toBeTruthy();
+    });
+  });
+});
+
+// Erreur 404 et Erreur 500
+
+describe("When an error occurs on API", () => {
+  beforeEach(() => {
+    jest.spyOn(mockStore, "bills");
+  });
+
+  test("fetches bills from an API and fails with 404 message error", async () => {
+    mockStore.bills.mockImplementationOnce(() => {
+      return {
+        list: () => {
+          return Promise.reject(new Error("Erreur 404"));
+        },
+      };
+    });
+    const pageContent = BillsUI({ error: "Erreur 404" });
+    document.body.innerHTML = pageContent;
+    const errorMessage = await screen.getByText(/Erreur 404/);
+    expect(errorMessage).toBeTruthy();
+  });
+
+  test("fetches messages from an API and fails with 500 message error", async () => {
+    mockStore.bills.mockImplementationOnce(() => {
+      return {
+        list: () => {
+          return Promise.reject(new Error("Erreur 500"));
+        },
+      };
+    });
+    const pageContent = BillsUI({ error: "Erreur 500" });
+    document.body.innerHTML = pageContent;
+    const errorMessage = await screen.getByText(/Erreur 500/);
+    expect(errorMessage).toBeTruthy();
   });
 });
